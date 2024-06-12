31acf88a6f52715702d8178b1b98bdc4b8c89509
Trịnh Đoàn Hậu <trinhdoanhauu@gmail.com>
2024-06-12 23:58:55 +0700

    Delete QuanLyDiemRenLuyen directory

--- QuanLyDiemRenLuyen/src/main/webapp/resources/js/script.js
+++
@@ -1,8 +0,0 @@
-function deleteHoatDong (url) {
-    fetch(url, {method : 'delete'}).then(res => {
-        if(res.status ===204)
-            location.reload();
-        else 
-            alert("ERROR");
-    })
-}
